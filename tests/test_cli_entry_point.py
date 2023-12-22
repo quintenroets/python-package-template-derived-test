efcb481bd35234227c509a209816897efa00d3e9
quintenroets <github-action@actions-template-sync.noreply.github.com>
2023-12-22 03:13:15 +0000

    chore(template): merge template changes :up:

---
+++ tests/test_cli_entry_point.py
@@ -0,0 +1,5 @@
+from python_package_template_derived_test import cli
+
+
+def test_testing() -> None:
+    cli.entry_point()
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from python_package_template_derived_test import main
+
+
+def test_main() -> None:
+    main()
